cb234d7d5e6942e46f854794f90d94ff812c628f
dongnam-motel <ugotlove@gmail.com>
2025-04-11 07:15:16 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App';  // App.js에서 컴포넌트를 가져옵니다.
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')  // public/index.html의 root div에 렌더링
 );
