97dab91d354af5600790fbad1ab6ff9bb2de960d
dongnam-motel <ugotlove@gmail.com>
2025-04-11 08:24:06 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // 만약 Tailwind CSS 설정 파일이 있다면 포함
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
