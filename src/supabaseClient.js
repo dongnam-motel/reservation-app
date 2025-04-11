7883564143ca02f0c61bde9f3259f349e6ab0ac8
dongnam-motel <ugotlove@gmail.com>
2025-04-11 23:30:30 +0900

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://qjrujlohkglfmghctiue.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqcnVqbG9oa2dsZm1naGN0aXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMDg0ODQsImV4cCI6MjA1OTg4NDQ4NH0.lZHhfPjZ_CI6ERPjXvcf8657GcBraULMmAAIMfFzXms';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
