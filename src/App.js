2e9943b95eee4526619e1f486854392c942c4aa1
dongnam-motel <ugotlove@gmail.com>
2025-04-11 18:14:52 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-// src/App.js
 import React from 'react';
-import ReservationStatus from './ReservationStatus';
+import ReservationStatus from './ReservationStatus'; // 기존 예약 현황 컴포넌트
+import GoogleAuth from './GoogleAuth'; // 방금 만든 GoogleAuth 컴포넌트
 
 function App() {
   return (
     <div className="App">
+      <GoogleAuth />
       <ReservationStatus />
     </div>
   );
