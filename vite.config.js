fca9c4f9387b1dc97ec7f07a3480294cb77fde10
dongnam <ugotlove@gmail.com>
2025-04-13 11:23:34 +0900

    Fix: add rollup input for index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  root: '.', // 👉 명시적으로 루트를 현재 디렉토리로 설정
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
+  }
 })
