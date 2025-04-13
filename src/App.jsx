9311e33ec401f896cf102336c3b55cf728907838
dongnam <ugotlove@gmail.com>
2025-04-13 12:13:18 +0900

    fix: connect ReservationStatus to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
+import ReservationStatus from './ReservationStatus'
 
-export default function App() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>예약 현황판 앱</h1>
-    </div>
-  )
+function App() {
+  return <ReservationStatus />
 }
+
+export default App
